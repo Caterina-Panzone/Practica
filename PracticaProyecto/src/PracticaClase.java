f0a7421719aa8a361da5752385f221984e4064c1
LABS\usuario02 <s.a.m_puan@hotmail.com>
2018-03-19 08:53:50 -0300

    Probando git con Stephi

--- PracticaProyecto/src/PracticaClase.java
+++ PracticaProyecto/src/PracticaClase.java
@@ -2,6 +2,6 @@
 public class PracticaClase {
 	  public static void main(String[] args) {
 
-	        System.out.println("Hello, World");
+	        System.out.println("Probando git con Stephanie");
 	    }
 }
