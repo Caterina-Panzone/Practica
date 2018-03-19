2e4cd6035a12e989d737b5b7df59e0b6c934df88
LABS\usuario01 <franco11-09@hotmail.com>
2018-03-19 08:56:45 -0300

    hola soy fran

--- PracticaProyecto/src/PracticaClase.java
+++ PracticaProyecto/src/PracticaClase.java
@@ -1,7 +1,8 @@
 
 public class PracticaClase {
 	  public static void main(String[] args) {
-
-	        System.out.println("Hello, World");
+			
+	        System.out.println("Probando git con Fran");
+			
 	    }
 }
